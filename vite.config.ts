48aa907205e46278440247e17cfb532a9cdf73a8
huy37204 <huy37204@gmail.com>
2025-05-08 15:48:23 +0700

    change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Điều này cho phép truy cập từ bên ngoài
+    port: 8081, // Port bạn muốn chạy
+    watch: {
+      usePolling: true,
+    },
+    strictPort: true, // Nếu 8082 bị chiếm, không tự động đổi port khác
+    hmr: {
+      host: "http://huytran3.workspace.opstech.org",
+      port: 8081,
+    },
+  },
+});
